59b13b734e2b81ae1da284cca8371ced89563325
daniel <zindade4@gmail.com>
2025-12-14 23:02:40 +0000

    trying developed home

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,14 +1,13 @@
-import './styles/App.css'
-import { Header } from './components/Header'
+import "./styles/App.css";
+import { Header } from "./components/Header";
+import Home from "./page/Home";
 
-function App() {
+export default function App() {
   return (
-    <>
-      <div>
-        <Header/>
-      </div>
-    </>
-  )
+    <div>
+      
+      <Header />
+      <Home />
+    </div>
+  );
 }
-
-export default App
---
+++ Frontend/src/page/Home.jsx
@@ -0,0 +1,10 @@
+import "../styles/index.css";
+
+export default function Home() {
+  return (
+    <main className="home">
+      <h1>Home</h1>
+      <p>Página inicial do e-commerce</p>
+    </main>
+  );
+}
