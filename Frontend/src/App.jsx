ee8ad390d37d721bc8105dda9b3a9ba0a119d5d6
daniel <zindade4@gmail.com>
2025-12-18 00:08:21 +0000

    a small part of footer just the begin and Home.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,13 +1,15 @@
 import "./styles/App.css";
 import { Header } from "./components/Header";
 import Home from "./page/Home";
+import { Footer } from "./components/Footer";
 
 export default function App() {
   return (
-    <div>
+    <div className="app-layout">
       
       <Header />
       <Home />
+      <Footer/>
     </div>
   );
 }
