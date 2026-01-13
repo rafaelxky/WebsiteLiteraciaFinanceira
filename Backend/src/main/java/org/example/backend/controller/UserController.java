7c17306b91b5ab7e4efd1d66d56f78d1c1c13559
rafael <rafapenacho@gmail.com>
2026-01-13 18:56:51 +0000

    User controller endpoint

--- Backend/src/main/java/org/example/backend/controller/UserController.java
+++ Backend/src/main/java/org/example/backend/controller/UserController.java
@@ -1,3 +1,8 @@
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/user")
 public class UserController {
     
 }
